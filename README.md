c41d1b42d68903d70e6247c638c84f9ebfe3283e
Pedro <pedromprodr@gmail.com>
2022-04-20 18:05:49 +0100

    Add final features

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# library-app
+# Library
+
+Library created with HTML, CSS and JS.
--- script.js
+++ script.js
@@ -13,7 +13,9 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
     content.appendChild(createBookElement(book));
 }
-
+function removeBook(bookElement){
+    content.removeChild(bookElement);
+}
 function createBookElement(book) {
     const bookContainer = document.createElement('div');
     bookContainer.className = "bookContainer";
@@ -26,45 +28,101 @@ function createBookElement(book) {
     const read = document.createElement('input')
     read.type = "checkbox";
     read.className = "toggle";
-    const readText =document.createElement('h4');
+    const readText = document.createElement('h4');
     readText.className = "readText";
     readText.textContent = "Finished!"
-  
-    
+
+    const deleteBook = document.createElement('a');
+    deleteBook.href="#";
+
+    let icon = document.createElement("img");
+    deleteBook.className="deleteIcon";
+    icon.setAttribute("src","./icons/delete_white_24dp.svg")
+    deleteBook.appendChild(icon);
+
+
 
     bookContainer.appendChild(bookAuthor);
     bookContainer.appendChild(bookName);
     bookContainer.appendChild(pages);
     bookContainer.appendChild(read);
-    if(book.read){
+    bookContainer.appendChild(deleteBook);
+
+    if (book.read) {
         bookContainer.appendChild(readText);
     }
-    
-    
 
     bookName.textContent = book.title;
     bookAuthor.textContent = book.author;
-    pages.textContent = book.pages+" pages.";
+    pages.textContent = book.pages + " pages.";
     read.checked = book.read;
 
     read.addEventListener('change', () => {
-        if(read.checked){
+        if (read.checked) {
             console.log('checked');
             book.read = true;
             bookContainer.appendChild(readText);
-        }else{
+        } else {
             book.read = false;
             bookContainer.removeChild(readText);
         }
     })
 
+    deleteBook.addEventListener("click", () =>{
+        removeBook(bookContainer);
+    })
+
     return bookContainer;
-    
+
 }
 
-const hungerGames = new Book("The Hunger Games","Suzanne Collins", 243, true);
+const hungerGames = new Book("The Hunger Games", "Suzanne Collins", 243, true);
 const milkAndHoney = new Book("Milk and Honey", "Rupi Kaur", 127, false);
 
 addBookToLibrary(hungerGames);
 addBookToLibrary(milkAndHoney);
+
+
+function openTheForm() {
+    document.getElementById("popupForm").style.display = "block";
+    content.style.opacity = "0";
+}
+
+let form = document.getElementById("bookForm");
+function handleForm(event) { event.preventDefault(); }
+form.addEventListener('submit', handleForm);
+
+
+let formToggle= false;
+document.querySelector("#read").addEventListener('change', () => {
+        formToggle=!formToggle;
+})
+
+function submitForm() {
+    let inputs = document.querySelectorAll(".form-input");
+    let valid = true;
+    inputs.forEach(element => { console.log(element.value); if (element.value == '' || element.value == null) valid = false;
+    });
+
+    if (valid) {
+        if(document.getElementById("read").value=='on') readInput = true
+        console.log("fun"+formToggle);
+        addBookToLibrary(new Book(document.getElementById("book-title").value, document.getElementById("book-author").value, document.getElementById("book-pages").value, formToggle))
+        closeForm();
+        form.reset();
+        formToggle=false;
+    } else{
+
+    }
+}
+
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+    content.style.opacity = "1";
+}
+
+document.getElementById("add").addEventListener('click', () => {
+    openTheForm();
+})
+
 
